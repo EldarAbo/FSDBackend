d4e9f7111e5f18d43e9c7215519acb6c18ae1b6e
arbeltzoran <arbel.tzoran.98@gmail.com>
2025-05-21 14:21:30 +0300

    notifications files

---
+++ models/notificationModel.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const notificationSchema = new mongoose.Schema({
+  subjectId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Subject",
+    required: true,
+  },
+  day: {
+    type: String,
+    enum: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+    required: true,
+  },
+  time: {
+    hour: { type: Number, required: true },
+    minute: { type: Number, required: true },
+  },
+  userId: {
+    type: String,
+    required: true,
+  },
+});
+
+const notificationModel = mongoose.model("Notification", notificationSchema);
+export default notificationModel;
